9b0ea9fa72d4f4d22df7fc9a2a2f31d4849357cb
Sara <sarajm1007@gmail.com>
2025-05-17 17:14:16 -0500

    Se agrega Asp_Servicios y Lib_Aplicaciones

---
+++ Asp_Servicios/Program.cs
@@ -0,0 +1,13 @@
+using Asp_servicio;
+using asp_servicios;
+using Microsoft.AspNetCore.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder, builder.Services);
+
+
+var app = builder.Build();
+startup.Configure(app, app.Environment);
+app.Run();
