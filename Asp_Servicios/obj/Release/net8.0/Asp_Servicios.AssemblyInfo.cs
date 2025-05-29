3b25ac4cd11e78972a652d1affb179589c2ddf4c
Sara <sarajm1007@gmail.com>
2025-05-29 09:14:09 -0500

    Nuevos proyectos

--- Asp_Servicios/obj/Release/net8.0/Asp_Servicios.AssemblyInfo.cs
+++ Asp_Servicios/obj/Release/net8.0/Asp_Servicios.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp_Servicios")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06948e814485f1c22e4b4a6c13140dbc4f588c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp_Servicios")]
